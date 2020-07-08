e8dcfa05e17f877f471d9edd4d2c223e15d7cca0
lesovsky <lesovsky@gmail.com>
2020-07-08 16:27:17 +0500

    Remove accidentally added test. Add code coverage test to .gitignore.

--- failconns/failconns_test.go
+++ failconns/failconns_test.go
@@ -2,7 +2,6 @@ package failconns
 
 import (
 	"context"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -19,17 +18,4 @@ func TestWorkload_Run(t *testing.T) {
 	w := NewWorkload(config)
 	err := w.Run(ctx)
 	assert.Nil(t, err)
-}
-
-func TestExample(t *testing.T) {
-	s := make([]*int, 0, 10)
-	fmt.Println("len: ", len(s))
-	fmt.Println("cap: ", cap(s))
-
-	a, b, c := 1, 2, 3
-	var x, y, z = &a, &b, &c
-
-	s = append(s, x, y, z)
-	fmt.Println("len: ", len(s))
-	fmt.Println("cap: ", cap(s))
 }
